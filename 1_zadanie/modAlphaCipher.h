08de560f96e49e86b05dd9f11e161c79e39dcd0e
Kirill0602 <113330070+KirillKond@users.noreply.github.com>
2023-03-27 18:19:52 +0300

    Update modAlphaCipher.h

--- 1_zadanie/modAlphaCipher.h
+++ 1_zadanie/modAlphaCipher.h
@@ -2,22 +2,19 @@
 #include <vector>
 #include <string>
 #include <map>
-#include <codecvt>
-#include <cstdlib>
 #include <locale>
 using namespace std;
 class modAlphaCipher
 {
-private://запрещает обращаться к свойствам вне класса
-    std::string numAlpha = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";//алфавит по порядку
-    wstring wnumAlpha;
-    std::map <wchar_t,int> alphaNum;//ассоциативный массив "номер по символу"
-    std::vector <int> key; //ключ
-    std::vector <int> convert(const std::wstring& s);//преобразование строка-вектор
-    std::wstring convert(const std::vector<int>& v);//преобразование вектор-строка
-public://даёт публичный доступ
-    modAlphaCipher()=delete; //запретим конструктор без параметров
-    modAlphaCipher(const std::wstring& skey); //конструктор для установки ключа
-    std::wstring encrypt(const std::wstring& open_text);//зашифрование
-    std::wstring decrypt(const std::wstring& cipher_text);//зашифрование
+private:
+    wstring numAlpha =L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";//алфавит по порядку
+    map <char,int> alphaNum;//ассоциативный массив "номер по символу"
+    vector <int> key;//ключ
+    vector<int> convert(const wstring& s); //преобразование строка-вектор
+    wstring convert(const vector<int>& v);//преобразование вектор-строка
+public:
+    modAlphaCipher()=delete;//запретим конструктор без параметров
+    modAlphaCipher(const wstring& skey);//конструктор для установки ключа
+    wstring encrypt(const wstring& open_text);//зашифрование
+    wstring decrypt(const wstring& cipher_text);//расшифрование
 };
