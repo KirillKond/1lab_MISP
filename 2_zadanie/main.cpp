49ddbe509ffd832344fbe901b5e3c95f95c1c475
Kirill0602 <113330070+KirillKond@users.noreply.github.com>
2023-03-31 12:44:05 +0300

    Update main.cpp

--- 2_zadanie/main.cpp
+++ 2_zadanie/main.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
 #include <cctype>
 #include "modAlphaCipher.h"
-#include <locale>
 using namespace std;
-bool isValid(const wstring& s)
+//проверка, чтобы строка состояла только из прописных букв
+bool isValid(const string& s)
 {
-    for(auto c:s)
-        if (!iswalpha(c) || !iswupper(c))
-            return false;
     return true;
 }
 int main(int argc, char **argv)
 {
-    int klych=2;
-    locale loc("ru_RU.UTF-8");
-    locale::global(loc);
-    wstring z;
-    wstring text;
+    int key;
+    string text;
     unsigned op;
-    Cipher cipher(klych);
+    cout<<"Cipher ready. Input key: ";
+    cin>>key;
+    cout<<"Key loaded\n";
+    modAlphaCipher cipher(key);
     do {
-        wcout<<L"Какую операцию необходимо произветси(0-exit, 1-encrypt, 2-decrypt): ";
-        wcin>>op;
+        cout<<"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
+            cin>>op;
+            if (op > 2) {
+        cout<<"Illegal operation\n";
+    } else if (op > 0) {
+        cout<<"Cipher ready. Input text: ";
+        cin>>text;
         if (isValid(text)) {
-            if (op >0) {
-            wcout<<L"Введите текст: ";
-            wcin>>text;
                 if (op==1) {
-                    wcout<<L"Закодированный текст: "<<cipher.zakodirovatCipher(cipher, text)<<endl;
+                    cout<<"Encrypted text: "<<cipher.encrypt(text)<<endl;
                 } else {
-                    wcout<<L"Раскодированный текст: "<<cipher.raskodirovatCipher(cipher, text)<<endl;
+                    cout<<"Decrypted text: "<<cipher.decrypt(text)<<endl;
                 }
-                }
+            } else {
+                cout<<"Operation aborted: invalid text\n";
+            }
         }
     } while (op!=0);
     return 0;
